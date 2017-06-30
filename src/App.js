0316c024b050b0995b19b05a30ffcf9317c45346
barossio <io.baross.io@gmail.com>
2017-06-30 16:03:36 +0700

    fin

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
     const store = createStore(reducers);
     return (
         <Provider store={store}>
-            <View>
-            <Text> tech stack </Text>
+            <View style={{ flex: 1 }}>
+                <Header headerText="Tech Stack" />
+                <LibraryList />
             </View>
         </Provider>
     );
